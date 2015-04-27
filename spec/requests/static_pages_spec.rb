da682320556b3514166f729dcfceed122bd90230
Kevin Leng <ldh2068vip@gmail.com>
2015-04-28 01:44:27 +0800

    Make a basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -51,7 +51,7 @@ describe "StaticPages" do
 subject { page }
   describe "Home page" do
     before { visit root_path }
-    it { should have_content('Sample App') }
+    it { should have_content('RedMax') }
     it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
 end
